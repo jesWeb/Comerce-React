3e76f704bfe0dbc6573cf9efa05200e61c6ba1f8
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-03-30 13:12:33 -0600

    implementamos el mapaeo de los items a mostrar en la pagina principal y este se mapea sobre un componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import Automovil from "./components/automovil";
 import Header from "./components/header";
+import { db } from "./data/db";
 
 function App() {
+  const data = db;
+  console.log("====================================");
+  console.log(data);
+  console.log("====================================");
+
   return (
     <>
       <Header />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Automóviles disponibles</h2>
-        <div className="row mt-5"></div>
-
-        <Automovil />
+        <div className="row mt-5">
+          {data.map((automovil) => (
+            <Automovil key={automovil.id} automovil={automovil} />
+          ))}
+        </div>
       </main>
 
       <footer className="mt-5 py-5">
--- src/components/Automovil.jsx
+++ src/components/Automovil.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const Automovil = () => {
+const Automovil = ({ automovil }) => {
+  //manejo de props de otra forma
+  const { name, image, brand, price, topSpeed, year } = automovil;
+
   return (
     <>
       <div className="col-md-8 col-lg-4 my-4 row align-items-center mx-auto">
         <div className="col-10 row align-items-center mx-auto">
           <img
             className="img-fluid"
-            src="./public/img/aspark owl.jpg"
+            src={[`/img/${image}.jpg`]}
             alt="imagen automóvil"
           />
-          <h3 className="text-black fs-4 fw-bold text-uppercase">
-            Bugatti Chiron
-          </h3>
-          <p>Bugatti</p>
-          <p>420 km/h</p>
-          <p>2016</p>
-          <p className="fw-black text-primary fs-3">U$S - 3.300.000</p>
+          <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+          <p>{brand}</p>
+          <p>Velocidad Maxima :{topSpeed}</p>
+          <p>{year}</p>
+          <p className="fw-black text-primary fs-3">
+            U$S - {price.toLocaleString()}
+          </p>
           <button type="button" className="btn btn-dark w-100">
             Agregar al Carrito
           </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Automovil
+export default Automovil;
